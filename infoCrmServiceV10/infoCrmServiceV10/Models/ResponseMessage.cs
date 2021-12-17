c3d6fb244460d0342766286149c09f8c2e08ff16
Tahir Yıldırım <93740858+MTY19@users.noreply.github.com>
2021-12-17 03:18:27 +0300

    All files

---
+++ infoCrmServiceV10/infoCrmServiceV10/Models/ResponseMessage.cs
@@ -0,0 +1,37 @@
+﻿namespace infoCrmServiceV10.Models
+{
+    public class ResponseMessage
+    {
+        public string AccountSid { get; set; }
+
+        public string? ApiVersion { get; set; }
+
+        public string? Body { get; set; }
+
+        public string? From { get; set; }
+
+        public string? MessageSid { get; set; }
+
+        public string? NumMedia { get; set; }
+
+        public string? NumSegments { get; set; }
+
+        public string? ProfileName { get; set; }
+
+        public string? SmsMessageSid { get; set; }
+
+        public string? SmsSid { get; set; }
+
+        public string? SmsStatus { get; set; }
+
+        public string? To { get; set; }
+
+        public string? WaId { get; set; }
+
+        public string? MediaContentType0 { get; set; }
+
+        public string? MediaUrl0 { get; set; }
+
+
+    }
+}
